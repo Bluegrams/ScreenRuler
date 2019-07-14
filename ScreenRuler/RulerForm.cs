c60a06ad453518d4129c607f5583194aeb2f4729
alxnull <26907770+alxnull@users.noreply.github.com>
2019-07-14 15:30:45 +0200

    Added Fody Costura.

--- ScreenRuler/RulerForm.cs
+++ ScreenRuler/RulerForm.cs
@@ -81,6 +81,7 @@ namespace ScreenRuler
                     break;
             }
             // Check for updates
+            manager.UpdateNotifyMode = Bluegrams.Application.UpdateNotifyMode.Always;
             Task.Run(() =>
             {
                 manager.CheckForUpdates("https://screenruler.sourceforge.io/update.xml");
