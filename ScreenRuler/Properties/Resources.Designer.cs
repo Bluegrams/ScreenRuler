1945a90402139971665d9ce79a90cf23cb293748
alxnull <alxnull@e.mail.de>
2022-09-04 00:47:03 +0200

    Update Spanish translation. Credit: https://www.martindrudi.xyz/.

--- ScreenRuler/Properties/Resources.Designer.cs
+++ ScreenRuler/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace ScreenRuler.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -61,7 +61,7 @@ namespace ScreenRuler.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Monitor DPI / Scaling: {0} / {1}%.
+        ///   Looks up a localized string similar to Scaling: {0}.
         /// </summary>
         internal static string ScalingText {
             get {
